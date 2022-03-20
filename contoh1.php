96d0b0c0f6d23d3ac1386b73d6eef1ca7d253e47
MRamziR0342 <ramziramadhan72@gmail.com>
2022-03-20 09:09:07 +0700

    LATIHAN1 WEB2

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya MUHAMMAD RAMZI RAMADHAN
+ Saya tinggal di daerah DEPOK
+ olah raga yang saya sukai adalah
+ SEPAK BOLA";
+ }
+}
